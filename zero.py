61acdf5ea7e0fce8710b08cf754742dd0c65b638
Ping <jiang_kangping@163.com>
2025-04-09 03:18:30 +0200

    and images are fine as well

--- zero.py
+++ zero.py
@@ -1,5 +1,17 @@
 import polars as pl
+from PIL import Image
+from io import BytesIO
 
 df = pl.read_parquet('/home/jkp/Téléchargements/zerobench_subquestions-00000-of-00001.parquet')
 
 print(df.head())
+
+
+first_row = df.row(0, named=True)
+images = first_row['question_images_decoded'] # list of images
+
+for image in images:
+    print(image.keys())
+    image = Image.open(BytesIO(image['bytes']))
+    image.show()
+
